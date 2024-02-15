e85b1a3f0913961f7a3edbcf2dd8bd9f4424e030
Fabrizio Silvestri <fabri.silve.fs@gmail.com>
2024-02-15 18:08:14 +0100

    add component for form about tokens

---
+++ src/components/SettingsModal.jsx
@@ -0,0 +1,77 @@
+import {
+  Button,
+  Dialog,
+  DialogPanel,
+  TextInput,
+} from '@tremor/react';
+import {
+  useState,
+  useCallback,
+} from 'react';
+import Cookies from 'js-cookie';
+
+const URL = "https://bbmtaxqvndfucwmecqlmu5a6bm0axqes.lambda-url.eu-central-1.on.aws";
+
+function SettingModal() {
+  const storedToken = Cookies.get('token');
+  const storedDatabase = Cookies.get('database');
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [token, setToken] = useState(storedToken || '');
+  const [database, setDatabase] = useState(storedDatabase || '');
+
+  const saveSettings = useCallback(async (event) => {
+    event.preventDefault();
+    Cookies.set('token', token, { expires: 30 });
+    Cookies.set('database', database, { expires: 30 });
+
+    let data = await fetch(`${URL}?page_size=100`);
+    let result = await data.json();
+    let tickets = result.pages.results;
+    let nextCursor = result.pages.next_cursor
+    while (nextCursor) {
+      data = await fetch(`${URL}?page_size=100&cursor=${nextCursor}`);
+      result = await data.json();
+      tickets = tickets.concat(result.pages.results);
+      nextCursor = result.pages.next_cursor
+    }
+
+    console.log(tickets);
+
+    setIsOpen(false)
+  }, [token, database]);
+
+  return (
+    <>
+      <Button className="mx-auto block" onClick={() => setIsOpen(true)}>Settings</Button>
+      <Dialog open={isOpen} onClose={(val) => setIsOpen(val)} static={true}>
+        <DialogPanel>
+          <h3 className="text-lg font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">Settings</h3>
+          <p className="mt-2 leading-6 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+            Populate the required fields to give access to the data.
+          </p>
+          <TextInput
+            className="mt-6"
+            placeholder="Notion Key"
+            type="password"
+            value={token}
+            onChange={(event) => setToken(event.target.value)}
+          />
+          <TextInput
+            className="mt-6"
+            placeholder="Database ID"
+            type="password"
+            value={database}
+            onChange={(event) => setDatabase(event.target.value)}
+          />
+          <Button className="mt-8 w-full" onClick={saveSettings}>
+            Fetch Data
+          </Button>
+        </DialogPanel>
+      </Dialog>
+    </>
+  );
+}
+
+export default SettingModal;
